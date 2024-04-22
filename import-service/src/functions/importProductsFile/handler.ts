f6595a8ff14f345ea123fd0694845fbe55620fc3
Serhii_Yudin <serhii.yudin@regeneron.com>
2024-04-22 21:52:58 +0300

    fixed method

--- import-service/src/functions/importProductsFile/handler.ts
+++ import-service/src/functions/importProductsFile/handler.ts
@@ -1,11 +1,11 @@
-import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 export const importProductsFile = async ({ queryStringParameters }) => {
   const client = new S3Client({ region: "eu-central-1" });
   const fileName = queryStringParameters.name;
   const path = `uploaded/${fileName}`;
-  const command = new GetObjectCommand({
+  const command = new PutObjectCommand({
     Bucket: "yudin-aws-import-service",
     Key: path,
   });
